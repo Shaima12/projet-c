5da8d853426bdb18e71dd3ee08b926dc88e9d25a
ichaoui chaima <cichaoui05@gmail.com>
2024-04-01 20:37:19 +0000

    second commit

---
+++ test/temp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    static char template[] = "/mnt/c/Users/cicha/OneDrive/Bureau/projet c/myfileXXXXXX";
+    char fname[1000];
+    strcpy(fname, template);
+    int fd = mkstemp(fname);
+    printf("Le chemin du fichier temporaire est : %s\n", fname);
+
+    return 0;
+}
+
